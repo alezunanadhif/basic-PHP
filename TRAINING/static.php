e44a28cce2baddf146b533a25e11cfb746e1933d
Alezuna Nadhif <110968215+AlezunaNadhif@users.noreply.github.com>
2023-01-27 14:47:04 +0700

    ap tu det

--- TRAINING/static.php
+++ TRAINING/static.php
@@ -9,21 +9,44 @@ class contohStatic {
     }
 }
 
-echo contohStatic::$angka;
-echo PHP_EOL;
-echo contohStatic::halo();
-echo PHP_EOL;
-echo contohStatic::halo();
-echo PHP_EOL;
-echo contohStatic::halo();
-echo PHP_EOL;
+// echo contohStatic::$angka . PHP_EOL;
 
-$contoh1 = new contohStatic;
-echo $contoh1->halo() . PHP_EOL;
-echo $contoh1->halo() . PHP_EOL;
-echo $contoh1->halo() . PHP_EOL;
+// echo contohStatic::halo() . PHP_EOL;
+// echo contohStatic::halo() . PHP_EOL;
+// echo contohStatic::halo() . PHP_EOL;
 
-$contoh2 = new contohStatic;
-echo $contoh2->halo() . PHP_EOL;
-echo $contoh2->halo() . PHP_EOL;
-echo $contoh2->halo() . PHP_EOL;
+// $contoh1 = new contohStatic;
+// echo $contoh1->halo() . PHP_EOL;
+// echo $contoh1->halo() . PHP_EOL;
+// echo $contoh1->halo() . PHP_EOL;
+
+// $contoh2 = new contohStatic;
+// echo $contoh2->halo() . PHP_EOL;
+// echo $contoh2->halo() . PHP_EOL;
+// echo $contoh2->halo() . PHP_EOL;
+
+class StringMutator
+{
+public static function bold(string $word): string
+{
+return sprintf('<b>%s</b>', $word);
+}
+public static function italic(string $word): string
+{
+return sprintf('<i>%s</i>', $word);
+}
+public static function boldItalic(string $word): string
+{
+return self::italic(self::bold($word));
+}
+}
+class ChildStringMutator extends StringMutator
+{
+public static function bold(string $word): string
+{
+return '<b>STATIC LATE BINDINGS</b>';
+}
+}
+
+echo ChildStringMutator::boldItalic('Muhamad Surya Iksanudin') . PHP_EOL;
+echo StringMutator::italic("RAKUY") . PHP_EOL;
