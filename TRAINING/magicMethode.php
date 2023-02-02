8324e33f8ac6d386202bf7f222a86bff6428e612
Alezuna Nadhif <110968215+AlezunaNadhif@users.noreply.github.com>
2023-02-02 14:02:55 +0700

    Magic Method

--- TRAINING/magicMethode.php
+++ TRAINING/magicMethode.php
@@ -13,15 +13,40 @@ class MagicMethod
         $this->age = $value;
         echo "Selamat Anda Berhasil Input $property dengan Data : $this->age" . PHP_EOL;
     } else {
-    throw new ParseError(sprintf('Undefined property
-    %s in class %s', $property, __CLASS__));
+    throw new ParseError(sprintf('Undefined property %s in class %s', $property, __CLASS__));
     }
 }
-    public function getNama() {
-        return $this->name;
+    public function getName() {
+        return $this->name . PHP_EOL;
+    }
+
+    public function getAge() {
+        return $this->age . PHP_EOL;
+    }
+
+    public function __get($property)
+    {
+        if($property !== null) {
+            return $this->$property . PHP_EOL;
+        } else {
+            throw new ParseError(sprintf('Undefined property %s in class %s', $property, __CLASS__));
+        }
     }
 }
 
 $magic = new MagicMethod();
 $magic->name = 'Muhammad Surya Iksanudin';
-echo $magic->getNama();
+$magic->age = 17;
+echo $magic->name;
+echo $magic->age;
+echo $magic->getName();
+echo $magic->getAge();
+$magic->ppp = "Ppp";
+
+try {
+    $magic->ppp;
+} catch (ParseError $p) {
+    echo $p->getMessage();
+} 
+
+echo $magic->getName();
