752eb2c28a61c61cc3900d81ccba397f232a4e48
Alezuna Nadhif <110968215+AlezunaNadhif@users.noreply.github.com>
2023-02-07 11:33:09 +0700

    composer

--- PPH21/index.php
+++ PPH21/index.php
@@ -8,5 +8,10 @@ $third = new ThirdRuleCalculator($second);
 $fourth = new FourthRuleCalculator($third);
 $calculator = new PPH21Calculator($first, $second, $third, $fourth);
 
-$penghasilanAkhir = $calculator->calculate(70000000);
+$gaji = 70000000;
+$pph21 = $calculator->calculate($gaji);
+$penghasilanAkhir = $gaji - $pph21;
+
+echo "Penghasilan Awal Rp. " . $gaji . PHP_EOL;
+echo "Pajak PPH21 Rp. " . number_format($pph21) . PHP_EOL;
 echo "Penghasilan Akhir Rp. " . number_format($penghasilanAkhir) . PHP_EOL;
